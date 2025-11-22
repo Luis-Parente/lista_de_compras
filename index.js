4a0ff2f6f0c993e12b6a1ce0d83fd00761a6c17f
Luís Parente <parente.lgsp@outlook.com>
2025-11-22 12:48:15 -0300

    adicionada função para adicionar novo item na lista

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const inputItem = document.getElementById("input-item")
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const adicionarItem = document.getElementById("adicionar-item");
 
-console.log(inputItem.value);
+let contador = 0;
+
+adicionarItem.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+        alert("Não pode adicionar item vazio!")
+        return;
+    }
+
+    const itemDaLista = document.createElement("li");
+
+    const containerLista = document.createElement("div");
+    containerLista.classList.add("lista-item-container");
+
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox" + contador++;
+
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerLista.appendChild(inputCheckbox);
+    containerLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerLista);
+
+    listaDeCompras.appendChild(itemDaLista);
+});
